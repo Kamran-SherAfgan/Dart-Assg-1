cc6a40743e7740ad98e2a4d212d919f711d091f4
Kamran Sher Afgan <64993556+Kamran-SherAfgan@users.noreply.github.com>
2021-05-14 19:19:18 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       home: (
+         Scaffold ( body: Text("Name: Kamran Sher Afgan , Email: kami.sher.ks@gmail.com"))
+      ),
+    );
+  }
+}
+   
